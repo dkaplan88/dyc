15ec83ca4b5c6459d6c900392a9ab616ec9fc572
Daniel Kaplan <daniel.scott.kaplan@gmail.com>
2012-05-16 15:56:43 -0500

    added images for events and arist page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,7 @@ class EventsController < ApplicationController
   respond_to :html, :json
   def index
     @ip = request.ip
+
     @city = request.location.city
     @city.blank? ? @city = "Chicago" : @city
     # if @city.blank?
@@ -14,8 +15,9 @@ class EventsController < ApplicationController
     # @lastfm = Lastfm.new("56d33663d228d59052676ff139ba3649", "803ad83bd5299a436c8c09b087a27d17")
        # @token = @lastfm.auth.get_token
        # @lastfm.session = @lastfm.auth.get_session(@token)['key']
-    @events = @lastfm.geo.get_events @city
+    @events = @lastfm.geo.get_events(@city,0,2) 
     respond_with @events 
+    
     # respond_to do |format| 
     #      format.html 
     #      format.json {render json: @events}
